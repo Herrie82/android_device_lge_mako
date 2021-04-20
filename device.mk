23c30697d61b573b7b5bf7721b51b42d1a7d19d2
Herrie <Github.com@herrie.org>
2021-04-20 19:58:01 +0200

    device.mk: Fix vibrator build

--- device.mk
+++ device.mk
@@ -259,7 +259,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vibrator.mako \
+    vibrator.default \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
